087cc5b9e010d69180070987e93c4910ce12f121
jjb8966 <jjb8966@naver.com>
2022-06-27 20:55:33 +0900

    feat: 유기동물 상세보기 페이지 구현 - ListAbandonedAnimalService를 AbandonedAnimalService로 이름 변경. 하나의 service 클래스로 AbandonedAnimal 관련 로직 수행. - form 디렉토리에서 폼 관련 데이터 관리하도록 변경

--- src/main/java/abandoned_animal/dao/AbandonedAnimalDao.java
+++ src/main/java/abandoned_animal/dao/AbandonedAnimalDao.java
@@ -1,7 +1,7 @@
 package abandoned_animal.dao;
 
+import abandoned_animal.form.AbandonedAnimalDetailForm;
 import abandoned_animal.model.AbandonedAnimal;
-import abandoned_animal.service.ListAbandonedAnimalService;
 import jdbc.JdbcUtil;
 
 import java.sql.*;
@@ -10,14 +10,52 @@ import java.util.List;
 
 public class AbandonedAnimalDao {
 
+    public AbandonedAnimalDetailForm selectOneAnimal(Connection conn, int abNumber) {
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        String sql = "select * from ABANDONEDANIMAL where ABNUMBER =" + abNumber;
+
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+
+            return makeDetailForm(rs);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private AbandonedAnimalDetailForm makeDetailForm(ResultSet rs) throws SQLException {
+        while (rs.next()) {
+            int abNumber = rs.getInt("ABNUMBER");
+            int sNumber = rs.getInt("SNUMBER");
+            int age = rs.getInt("AGE");
+            float weight = rs.getFloat("WEIGHT");
+            String name = rs.getString("NAME");
+            String species = rs.getString("SPECIES");
+            String kind = rs.getString("KIND");
+            String gender = rs.getString("GENDER");
+            String location = rs.getString("LOCATION");
+            String uniqueness = rs.getString("UNIQUENESS");
+            String description = rs.getString("DESCRIPTION");
+            String imgPath = rs.getString("IMGPATH");
+            String animalState = rs.getString("ANIMALSTATE");
+            Date admissionDate = rs.getDate("ADMISSIONDATE");
+
+            return new AbandonedAnimalDetailForm(abNumber, sNumber, age, weight, name, species, kind, gender, location, uniqueness, description, imgPath, animalState, admissionDate);
+        }
+        return null;
+    }
+
     public List<AbandonedAnimal> selectIndex(Connection conn, int start, int end) throws SQLException {
         PreparedStatement pstmt = null;
 //        Statement statement = null;
         ResultSet rs = null;
 
-        String sql = "select NAME, LOCATION, ADMISSIONDATE, IMGPATH " +
+        String sql = "select ABNUMBER, NAME, LOCATION, ADMISSIONDATE, IMGPATH " +
                 "FROM " +
-                "(select rownum, NAME, LOCATION, ADMISSIONDATE, IMGPATH from ABANDONEDANIMAL) " +
+                "(select rownum, ABNUMBER, NAME, LOCATION, ADMISSIONDATE, IMGPATH from ABANDONEDANIMAL) " +
                 "where rownum between ? and ?";
 
         try {
@@ -43,23 +81,6 @@ public class AbandonedAnimalDao {
         }
     }
 
-    private AbandonedAnimal convertAbandonedAnimal(ResultSet rs) throws SQLException {
-        String name = rs.getString("NAME");
-        String img = rs.getString("IMGPATH");
-        String location = rs.getString("LOCATION");
-        Date admissionDate = rs.getDate("ADMISSIONDATE");
-
-        return new AbandonedAnimal(name, img, location, admissionDate);
-//        return new AbandonedAnimal(rs.getInt("article_no"),
-//                new Writer(
-//                        rs.getString("writer_id"),
-//                        rs.getString("writer_name")),
-//                rs.getString("title"),
-//                toDate(rs.getTimestamp("regdate")),
-//                toDate(rs.getTimestamp("moddate")),
-//                rs.getInt("read_cnt"));
-    }
-
     public int selectCount(Connection conn) throws SQLException {
         Statement stmt = null;
         ResultSet rs = null;
@@ -77,5 +98,23 @@ public class AbandonedAnimalDao {
             JdbcUtil.close(rs);
             JdbcUtil.close(stmt);
         }
+    }
+
+    private AbandonedAnimal convertAbandonedAnimal(ResultSet rs) throws SQLException {
+        int abNumber = rs.getInt("ABNUMBER");
+        String name = rs.getString("NAME");
+        String img = rs.getString("IMGPATH");
+        String location = rs.getString("LOCATION");
+        Date admissionDate = rs.getDate("ADMISSIONDATE");
+
+        return new AbandonedAnimal(abNumber, name, img, location, admissionDate);
+//        return new AbandonedAnimal(rs.getInt("article_no"),
+//                new Writer(
+//                        rs.getString("writer_id"),
+//                        rs.getString("writer_name")),
+//                rs.getString("title"),
+//                toDate(rs.getTimestamp("regdate")),
+//                toDate(rs.getTimestamp("moddate")),
+//                rs.getInt("read_cnt"));
     }
 }
--- src/main/java/abandoned_animal/service/AbandonedAnimalService.java
+++ src/main/java/abandoned_animal/service/AbandonedAnimalService.java
@@ -1,6 +1,8 @@
 package abandoned_animal.service;
 
 import abandoned_animal.dao.AbandonedAnimalDao;
+import abandoned_animal.form.AbandonedAnimalDetailForm;
+import abandoned_animal.form.AbandonedAnimalPage;
 import abandoned_animal.model.AbandonedAnimal;
 import jdbc.connection.ConnectionProvider;
 
@@ -8,10 +10,23 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
-public class ListAbandonedAnimalService {
+public class AbandonedAnimalService {
 
     private AbandonedAnimalDao abandonedAnimalDao = new AbandonedAnimalDao();
     private int size = 12;
+
+    public AbandonedAnimalDetailForm getDetailForm(int abNumber) {
+        try (Connection conn = ConnectionProvider.getConnection()) {
+            AbandonedAnimalDetailForm detailForm = abandonedAnimalDao.selectOneAnimal(conn, abNumber);
+
+            return detailForm;
+//            List<AbandonedAnimal> content = abandonedAnimalDao.selectIndex(conn, (pageNo - 1) * size, size);
+//
+//            return new AbandonedAnimalPage(total, pageNo, size, content);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public AbandonedAnimalPage getAbandonedAnimalPage(int pageNo) {
         try (Connection conn = ConnectionProvider.getConnection()) {
