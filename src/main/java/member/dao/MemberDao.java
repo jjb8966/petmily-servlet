57cb468243130a78784cba1a3647d5ebc0d0f824
jjb8966 <jjb8966@naver.com>
2022-06-30 22:45:44 +0900

    feat: selectByMNumber 메소드 추가

--- src/main/java/member/dao/MemberDao.java
+++ src/main/java/member/dao/MemberDao.java
@@ -5,8 +5,6 @@ import member.model.Member;
 
 import java.sql.*;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.sql.Date;
 
 public class MemberDao {
 
@@ -40,6 +38,37 @@ public class MemberDao {
 		}
 	}
 
+	public Member selectByMNumber(Connection conn, int mNumber) throws SQLException {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			pstmt = conn.prepareStatement(
+					"select * from member where MNUMBER = ?");
+			pstmt.setInt(1, mNumber);
+			rs = pstmt.executeQuery();
+			Member member = null;
+
+			if (rs.next()) {
+				member = new Member(rs.getInt("MNUMBER"),
+						rs.getString("ID"),
+						rs.getString("PW"),
+						rs.getString("NAME"),
+						rs.getDate("BIRTH"),
+						rs.getString("GENDER"),
+						rs.getString("EMAIL"),
+						rs.getString("PHONE"),
+						rs.getString("GRADE"));
+//						toDate(rs.getTimestamp("regdate")));
+			}
+
+			return member;
+		} finally {
+			JdbcUtil.close(rs);
+			JdbcUtil.close(pstmt);
+		}
+	}
+
 	public void insert(Connection conn, Member mem) throws SQLException, ParseException {
 		try (PreparedStatement pstmt = conn.prepareStatement("insert into MEMBER (ID, PW, NAME, BIRTH, GENDER, EMAIL, PHONE) values (?,?,?,?,?,?,?)")) {
 			pstmt.setString(1, mem.getId());
@@ -59,6 +88,14 @@ public class MemberDao {
 			pstmt.setString(1, member.getName());
 			pstmt.setString(2, member.getPw());
 			pstmt.setString(3, member.getId());
+			pstmt.executeUpdate();
+		}
+	}
+
+	public void delete(Connection conn, int mNumber) throws SQLException {
+		try (PreparedStatement pstmt = conn.prepareStatement(
+				"delete MEMBER where MNUMBER=?")) {
+			pstmt.setInt(1, mNumber);
 			pstmt.executeUpdate();
 		}
 	}
