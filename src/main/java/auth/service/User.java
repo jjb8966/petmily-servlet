f23f6915e6a0c3883b9b745d58bdf9b994d1f3fb
jjb8966 <jjb8966@naver.com>
2022-06-30 22:42:02 +0900

    refactor: mNumber 필드 추가

--- src/main/java/auth/service/User.java
+++ src/main/java/auth/service/User.java
@@ -2,10 +2,12 @@ package auth.service;
 
 public class User {
 
+	private int mNumber;
 	private String id;
 	private String name;
 
-	public User(String id, String name) {
+	public User(int mNumber, String id, String name) {
+		this.mNumber = mNumber;
 		this.id = id;
 		this.name = name;
 	}
@@ -18,4 +20,7 @@ public class User {
 		return name;
 	}
 
+	public int getMNumber() {
+		return mNumber;
+	}
 }
