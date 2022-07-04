c612cac13adc2abf6f0322753e8f5ac3ed66f9c5
younghoon211@naver.com <younghoon211@naver.com>
2022-07-04 19:57:49 +0900

    feat: DonationDao.java 구현

---
+++ src/main/java/abandoned_animal/dao/DonationDao.java
@@ -0,0 +1,34 @@
+package abandoned_animal.dao;
+
+import jdbc.JdbcUtil;
+import member.model.Member;
+
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import abandoned_animal.model.Donation;
+
+public class DonationDao {
+
+	public void insert(Connection conn, Donation donation) {
+		PreparedStatement pstmt = null;
+		String sql = "insert into DONATION (DNUMBER, ABNUMBER, MNUMBER, DONASUM, BANK, ACCOUNTHOLDER, ACCOUNTNUMBER) values (?,?,?,?,?,?,?)";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, donation.getdNumber());
+			pstmt.setInt(2, donation.getAbNumber());
+			pstmt.setInt(3, donation.getmNumber());
+			pstmt.setInt(4, donation.getDonaSum());
+			pstmt.setString(5, donation.getBank());
+			pstmt.setString(6, donation.getAccountHolder());
+			pstmt.setString(7, donation.getAccountNumber());
+			pstmt.executeUpdate();
+		} 
+		catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			JdbcUtil.close(pstmt);
+		}
+	}
+}
