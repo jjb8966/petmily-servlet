4db781fca948f312aff922ddfe841e3bff3d2521
dmswl1570@hanmail.net <dmswl1570@hanmail.net>
2022-07-05 19:29:45 +0900

    style: 자바 컨벤션 준수 및 코드 정리

--- src/main/java/abandoned_animal/dao/AdoptTempDao.java
+++ src/main/java/abandoned_animal/dao/AdoptTempDao.java
@@ -15,15 +15,12 @@ public class AdoptTempDao {
 		String sql = "insert into ADOPT (MNUMBER, ABNUMBER, RESIDENCE, MARITALSTATUS, JOB) values (?,?,?,?,?)";
 
 		try {
-			// insert into~~
 			pstmt = conn.prepareStatement(sql);
-//		 	pstmt.setString(1, id);
 			pstmt.setInt(1, adopt.getmNumber());
 			pstmt.setInt(2, adopt.getAbNumber());
 			pstmt.setString(3, adopt.getResidence());
 			pstmt.setString(4, adopt.getMaritalStatus());
 			pstmt.setString(5, adopt.getJob());
-//			rs = pstmt.executeQuery();
 			pstmt.executeUpdate();
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
@@ -35,19 +32,14 @@ public class AdoptTempDao {
 	public void insertTemp(Connection conn, TempPet temp) {
 		PreparedStatement pstmt = null;
 		String sql = "insert into TEMPPET (ABNUMBER, MNUMBER, RESIDENCE, MARITALSTATUS, JOB, TEMPDATE, TEMPPERIOD) values (?,?,?,?,?, SYSDATE, 2)";
-		// insert into~~
-		// String sql;
 
 		try {
 			pstmt = conn.prepareStatement(sql);
-//			pstmt = conn.prepareStatement(sql);
-//			pstmt.setString(1, id);
 			pstmt.setInt(1, temp.getAbNumber());
 			pstmt.setInt(2, temp.getmNumber());
 			pstmt.setString(3, temp.getResidence());
 			pstmt.setString(4, temp.getMaritalStatus());
 			pstmt.setString(5, temp.getJob());
-//			rs = pstmt.executeQuery();
 			pstmt.executeUpdate();
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
