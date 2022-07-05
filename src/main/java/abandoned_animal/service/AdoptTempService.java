6399e1bc17831c92cd63aef495d41abcfcacab2e
dmswl1570@hanmail.net <dmswl1570@hanmail.net>
2022-07-05 19:46:02 +0900

    style: 자바 컨벤션 준수 및 코드 정리

--- src/main/java/abandoned_animal/service/AdoptTempService.java
+++ src/main/java/abandoned_animal/service/AdoptTempService.java
@@ -17,20 +17,18 @@ public class AdoptTempService {
 	private AdoptTempDao adoptTempDao = new AdoptTempDao();
 	private AbandonedAnimalDao abandonedAnimalDao = new AbandonedAnimalDao();
 	private MemberDao memberDao = new MemberDao();
-	
-	public void adopt(AdoptTempSubmitForm adoptTempSubmitForm) {
 
+	public void adopt(AdoptTempSubmitForm adoptTempSubmitForm) {
 		Connection conn = null;
+
 		try {
 			conn = ConnectionProvider.getConnection();
 			conn.setAutoCommit(false);
 
-			// Adopt adopt = new Adopt();
 			Adopt adopt = toAdopt(adoptTempSubmitForm);
 			adoptTempDao.insertAdopt(conn, adopt);
-			conn.commit();
-			// return 1;
 
+			conn.commit();
 		} catch (SQLException e) {
 			JdbcUtil.rollback(conn);
 			throw new RuntimeException(e);
@@ -38,32 +36,18 @@ public class AdoptTempService {
 			JdbcUtil.close(conn);
 		}
 	}
-//		try (Connection conn = ConnectionProvider.getConnection()) {
-//			Adopt adopt = new Adopt();
-//			
-//			adoptTempSubmitForm.get
-//			
-//			adoptTempDao.insertAdopt(conn, adopt);
-//
-//			return detailForm;
-//		
-//		} catch (SQLException e) {
-//			throw new RuntimeException(e);
-//		}
-//	};
 
 	public void tempProtect(AdoptTempSubmitForm adoptTempSubmitForm) {
 		Connection conn = null;
+
 		try {
 			conn = ConnectionProvider.getConnection();
 			conn.setAutoCommit(false);
 
-			// TempPet tempPet = new TempPet();
 			TempPet tempPet = toTempPet(adoptTempSubmitForm);
 			adoptTempDao.insertTemp(conn, tempPet);
-			conn.commit();
-			// return 1;
 
+			conn.commit();
 		} catch (SQLException e) {
 			JdbcUtil.rollback(conn);
 			throw new RuntimeException(e);
@@ -75,25 +59,25 @@ public class AdoptTempService {
 	public String findAnimalName(int abNumber) {
 		Connection conn = null;
 
-        try {
+		try {
 			conn = ConnectionProvider.getConnection();
-            String animalName = abandonedAnimalDao.selectName(conn, abNumber);
-            
-            return animalName;
-        } catch (SQLException e) {
-        	throw new RuntimeException(e);
-        } finally {
+			String animalName = abandonedAnimalDao.selectName(conn, abNumber);
+
+			return animalName;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
 			JdbcUtil.close(conn);
-        }
+		}
 	}
 
 	public String findMemberName(int mNumber) {
 		Connection conn = null;
-		
+
 		try {
 			conn = ConnectionProvider.getConnection();
 			String memberName = memberDao.selectName(conn, mNumber);
-			
+
 			return memberName;
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
@@ -113,17 +97,4 @@ public class AdoptTempService {
 				adoptTempSubmitForm.getResidence(), adoptTempSubmitForm.getMaritalStatus(),
 				adoptTempSubmitForm.getJob());
 	}
-
-//    public AdoptTempSubmitForm getSubmitForm(int abNumber) {
-//        try (Connection conn = ConnectionProvider.getConnection()) {
-//        	AdoptTempSubmitForm submitForm = AdoptDao.selectOneAnimal(conn, abNumber);
-//
-//            return detailForm;
-////            List<AbandonedAnimal> content = abandonedAnimalDao.selectIndex(conn, (pageNo - 1) * size, size);
-////
-////            return new AbandonedAnimalPage(total, pageNo, size, content);
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
 }
