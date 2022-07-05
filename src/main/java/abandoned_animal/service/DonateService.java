4529b8ca47f1d9ad4edaaf7bc086016ef58b6ed3
younghoon211@naver.com <younghoon211@naver.com>
2022-07-05 22:02:08 +0900

    feat: 유기동물 및 후원자 이름 find메소드 추가

--- src/main/java/abandoned_animal/service/DonateService.java
+++ src/main/java/abandoned_animal/service/DonateService.java
@@ -2,14 +2,20 @@ package abandoned_animal.service;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+
+import abandoned_animal.dao.AbandonedAnimalDao;
 import abandoned_animal.dao.DonationDao;
 import abandoned_animal.form.DonateSubmitForm;
 import abandoned_animal.model.Donation;
+import jdbc.JdbcUtil;
 import jdbc.connection.ConnectionProvider;
+import member.dao.MemberDao;
 
 public class DonateService {
 
 	private DonationDao donationDao = new DonationDao();
+	private AbandonedAnimalDao abandonedAnimalDao = new AbandonedAnimalDao();
+	private MemberDao memberDao = new MemberDao();
 
 	public void donate(DonateSubmitForm donateSubmitForm) {
 		Donation donation = new Donation(
@@ -25,6 +31,36 @@ public class DonateService {
 			donationDao.insert(conn, donation);
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
+		}
+	}
+	
+	public String findAnimalName(int abNumber) {
+		Connection conn = null;
+		
+		try {
+			conn = ConnectionProvider.getConnection();
+			String animalName = abandonedAnimalDao.selectName(conn, abNumber);
+			
+			return animalName;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			JdbcUtil.close(conn);
+		}
+	}
+	
+	public String findMemberName(int mNumber) {
+		Connection conn = null;
+
+		try {
+			conn = ConnectionProvider.getConnection();
+			String memberName = memberDao.selectName(conn, mNumber);
+
+			return memberName;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			JdbcUtil.close(conn);
 		}
 	}
 }
