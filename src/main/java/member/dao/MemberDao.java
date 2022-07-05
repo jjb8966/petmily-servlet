91b7f4afad04dd86f95e39e69ed73971f33b39bd
cmpra <cmpra@61.77.49.8>
2022-07-05 21:58:43 +0900

    feat: 봉사신청서 회원 정보 추가 출력 MemberDao 수정

--- src/main/java/member/dao/MemberDao.java
+++ src/main/java/member/dao/MemberDao.java
@@ -1,12 +1,16 @@
 package member.dao;
 
-import jdbc.JdbcUtil;
-import member.model.Member;
-
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+
+import jdbc.JdbcUtil;
+import member.MemberInfo;
+import member.model.Member;
 
 public class MemberDao {
 
@@ -15,33 +19,27 @@ public class MemberDao {
 		ResultSet rs = null;
 
 		try {
-			pstmt = conn.prepareStatement(
-					"select * from member where ID = ?");
+			pstmt = conn.prepareStatement("select * from member where ID = ?");
 			pstmt.setString(1, id);
 			rs = pstmt.executeQuery();
 			Member member = null;
 			if (rs.next()) {
-				member = new Member(rs.getString("ID"),
-						rs.getString("PW"),
-						rs.getString("NAME"),
-						rs.getDate("BIRTH"),
-						rs.getString("GENDER"),
-						rs.getString("EMAIL"),
-						rs.getString("PHONE"));
+				member = new Member(rs.getString("ID"), rs.getString("PW"), rs.getString("NAME"), rs.getDate("BIRTH"),
+						rs.getString("GENDER"), rs.getString("EMAIL"), rs.getString("PHONE"));
 //						toDate(rs.getTimestamp("regdate")));
-                member.setmNumber(rs.getInt("MNUMBER"));
+				member.setmNumber(rs.getInt("MNUMBER"));
 			}
 			return member;
-			
+
 		} finally {
 			JdbcUtil.close(rs);
 			JdbcUtil.close(pstmt);
 		}
 	}
 
-
 	public void insert(Connection conn, Member mem) throws SQLException, ParseException {
-		try (PreparedStatement pstmt = conn.prepareStatement("insert into MEMBER (ID, PW, NAME, BIRTH, GENDER, EMAIL, PHONE) values (?,?,?,?,?,?,?)")) {
+		try (PreparedStatement pstmt = conn.prepareStatement(
+				"insert into MEMBER (ID, PW, NAME, BIRTH, GENDER, EMAIL, PHONE) values (?,?,?,?,?,?,?)")) {
 			pstmt.setString(1, mem.getId());
 			pstmt.setString(2, mem.getPw());
 			pstmt.setString(3, mem.getName());
@@ -54,37 +52,126 @@ public class MemberDao {
 	}
 
 	public void update(Connection conn, Member member) throws SQLException {
-		try (PreparedStatement pstmt = conn.prepareStatement(
-				"update member set name = ?, pw = ? where id = ?")) {
+		try (PreparedStatement pstmt = conn.prepareStatement("update member set name = ?, pw = ? where id = ?")) {
 			pstmt.setString(1, member.getName());
 			pstmt.setString(2, member.getPw());
 			pstmt.setString(3, member.getId());
 			pstmt.executeUpdate();
 		}
 	}
-	
+
 	public String selectName(Connection conn, int mNumber) {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		
-		String sql ="select Name from MEMBER where MNUMBER = ?";
-		
+
+		String sql = "select Name from MEMBER where MNUMBER = ?";
+
 		try {
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, mNumber);
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				return rs.getString("NAME");
 			}
-			
+
 			return null;
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			throw new RuntimeException(e);
-		}finally {
+		} finally {
 			JdbcUtil.close(rs);
 			JdbcUtil.close(pstmt);
 		}
-		
 	}
+
+	public String selectBirth(Connection conn, int mNumber) {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		String sql = "select BIRTH from MEMBER where MNUMBER = ?";
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, mNumber);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				return rs.getString("BIRTH");
+			}
+
+			return null;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			JdbcUtil.close(rs);
+			JdbcUtil.close(pstmt);
+		}
+	}
+
+	public String selectPhone(Connection conn, int mNumber) {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		String sql = "select PHONE from MEMBER where MNUMBER = ?";
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, mNumber);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				return rs.getString("PHONE");
+			}
+
+			return null;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			JdbcUtil.close(rs);
+			JdbcUtil.close(pstmt);
+		}
+	}
+
+	public String selectEmail(Connection conn, int mNumber) {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		String sql = "select EMAIL from MEMBER where MNUMBER = ?";
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, mNumber);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				return rs.getString("EMAIL");
+			}
+
+			return null;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			JdbcUtil.close(rs);
+			JdbcUtil.close(pstmt);
+		}
+	}
+
+	/*
+	 * //리팩토링예정 public MemberInfo findMemberInfo(Connection conn, int mNumber) {
+	 * Statement stmt = null; ResultSet rs = null;
+	 * 
+	 * String sql = "select * from MEMBER where MNUMBER =" + mNumber;
+	 * 
+	 * try { stmt = conn.createStatement(); rs = stmt.executeQuery(sql);
+	 * 
+	 * return makeDetailForm(rs); } catch (SQLException e) { throw new
+	 * RuntimeException(e); } }
+	 * 
+	 * private MemberInfo makeDetailForm(ResultSet rs) throws SQLException { while
+	 * (rs.next()) { String name = rs.getString("NAME"); Date birth =
+	 * rs.getDate("BIRTH"); String phone = rs.getString("PHONE"); String email =
+	 * rs.getString("EMAIL");
+	 * 
+	 * return new Member(name, birth, phone, email); } return null; }
+	 */
 }
