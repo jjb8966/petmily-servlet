d055445eb1954d21d10a9a63d2f0221484aa7d2d
javac <javac@DESKTOP-RBJMJCS>
2022-07-05 18:06:09 +0900

    style: 자바 컨벤션 준수 및 코드 정렬

--- src/main/java/member/command/ChangeMemberInfoHandler.java
+++ src/main/java/member/command/ChangeMemberInfoHandler.java
@@ -14,14 +14,17 @@ import member.service.MemberService;
 import mvc.command.CommandHandler;
 
 public class ChangeMemberInfoHandler implements CommandHandler {
+	
 	private static final String FORM_VIEW = "/WEB-INF/view/member/changeMemberInfo.jsp";
 	private MemberService memberService = new MemberService();
 
     @Override
     public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
+    	
     	if (req.getMethod().equalsIgnoreCase("GET")) {
 			return processForm(req, res);
 		} else if (req.getMethod().equalsIgnoreCase("POST")) {
+			
 			return processSubmit(req, res);
 		} else {
 			res.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
@@ -38,13 +41,11 @@ public class ChangeMemberInfoHandler implements CommandHandler {
 		return FORM_VIEW;
 	}
 
-
 	private String processSubmit(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		User user = (User)req.getSession().getAttribute("authUser");
 		String id = user.getId();
 
         MemberInfo memberInfo = memberService.findById(id);
-
         req.setAttribute("memberInfo", memberInfo);	
 
 		Map<String, Boolean> errors = new HashMap<>();
@@ -56,23 +57,22 @@ public class ChangeMemberInfoHandler implements CommandHandler {
 		String phone = req.getParameter("phone");
 		Member newMemberInfo =new Member(pw,name,email,phone,id);
 
-			
 		if (!errors.isEmpty()) {
-			
 			return FORM_VIEW;
 		}
 		
 		try {
 			memberService.changeMemberInfo(user.getId(), newMemberInfo);
 			User newUser=new User(user.getId(),name);
-			
 	        MemberInfo updateInfo = memberService.findById(id);
-	        req.setAttribute("memberInfo", updateInfo);
 	        
+	        req.setAttribute("memberInfo", updateInfo);
 			req.getSession().setAttribute("authUser", newUser);
+			
 			return "/WEB-INF/view/member/mypage.jsp";
 		} catch (MemberNotFoundException e) {
 			res.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			
 			return null;
 		}
 	}
