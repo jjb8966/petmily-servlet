c7e2045a5c00d355615710028d9c585fbd995e6f
cmpra <cmpra@221.147.207.228>
2022-07-06 22:12:53 +0900

    refactor: MemberDao 클래스 메소드 내에서 SQLExcpetion 예외 처리하도록 수정

--- src/main/java/member/dao/MemberDao.java
+++ src/main/java/member/dao/MemberDao.java
@@ -1,16 +1,10 @@
 package member.dao;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-
 import jdbc.JdbcUtil;
-import member.MemberInfo;
 import member.model.Member;
+
+import java.sql.*;
+import java.text.ParseException;
 
 public class MemberDao {
 
@@ -19,27 +13,38 @@ public class MemberDao {
 		ResultSet rs = null;
 
 		try {
-			pstmt = conn.prepareStatement("select * from member where ID = ?");
+			pstmt = conn.prepareStatement(
+					"select * from member where ID = ?");
 			pstmt.setString(1, id);
 			rs = pstmt.executeQuery();
 			Member member = null;
 			if (rs.next()) {
-				member = new Member(rs.getString("ID"), rs.getString("PW"), rs.getString("NAME"), rs.getDate("BIRTH"),
-						rs.getString("GENDER"), rs.getString("EMAIL"), rs.getString("PHONE"));
-//						toDate(rs.getTimestamp("regdate")));
+				member = new Member(
+						rs.getString("ID"),
+						rs.getString("PW"),
+						rs.getString("NAME"),
+						rs.getDate("BIRTH"),
+						rs.getString("GENDER"),
+						rs.getString("EMAIL"),
+						rs.getString("PHONE")
+						);
 				member.setmNumber(rs.getInt("MNUMBER"));
 			}
+			
 			return member;
-
 		} finally {
 			JdbcUtil.close(rs);
 			JdbcUtil.close(pstmt);
 		}
 	}
 
-	public void insert(Connection conn, Member mem) throws SQLException, ParseException {
-		try (PreparedStatement pstmt = conn.prepareStatement(
-				"insert into MEMBER (ID, PW, NAME, BIRTH, GENDER, EMAIL, PHONE) values (?,?,?,?,?,?,?)")) {
+
+	public void insert(Connection conn, Member mem){
+		PreparedStatement pstmt = null;
+		String sql = "insert into MEMBER (ID, PW, NAME, BIRTH, GENDER, EMAIL, PHONE) values (?,?,?,?,?,?,?)";
+		
+		try  {
+			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, mem.getId());
 			pstmt.setString(2, mem.getPw());
 			pstmt.setString(3, mem.getName());
@@ -48,23 +53,35 @@ public class MemberDao {
 			pstmt.setString(6, mem.getEmail());
 			pstmt.setString(7, mem.getPhone());
 			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			JdbcUtil.close(pstmt);
 		}
 	}
 
-	public void update(Connection conn, Member member) throws SQLException {
-		try (PreparedStatement pstmt = conn.prepareStatement("update member set name = ?, pw = ? where id = ?")) {
+	public void update(Connection conn, Member member) {
+		PreparedStatement pstmt = null;
+		String sql = "update member set name = ?, pw = ? where id = ?";
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, member.getName());
 			pstmt.setString(2, member.getPw());
 			pstmt.setString(3, member.getId());
 			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			JdbcUtil.close(pstmt);
 		}
 	}
-
+	
 	public String selectName(Connection conn, int mNumber) {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 
-		String sql = "select Name from MEMBER where MNUMBER = ?";
+		String sql = "select NAME from MEMBER where MNUMBER = ?";
 
 		try {
 			pstmt = conn.prepareStatement(sql);
@@ -83,95 +100,94 @@ public class MemberDao {
 			JdbcUtil.close(pstmt);
 		}
 	}
-
+	
 	public String selectBirth(Connection conn, int mNumber) {
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
 
-		String sql = "select BIRTH from MEMBER where MNUMBER = ?";
+        String sql = "select BIRTH from MEMBER where MNUMBER = ?";
 
-		try {
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, mNumber);
-			rs = pstmt.executeQuery();
+        try {
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, mNumber);
+            rs = pstmt.executeQuery();
 
-			if (rs.next()) {
-				return rs.getString("BIRTH");
-			}
+            if (rs.next()) {
+                return rs.getString("BIRTH");
+            }
 
-			return null;
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		} finally {
-			JdbcUtil.close(rs);
-			JdbcUtil.close(pstmt);
-		}
-	}
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            JdbcUtil.close(rs);
+            JdbcUtil.close(pstmt);
+        }
+    }
 
-	public String selectPhone(Connection conn, int mNumber) {
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+    public String selectPhone(Connection conn, int mNumber) {
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
 
-		String sql = "select PHONE from MEMBER where MNUMBER = ?";
+        String sql = "select PHONE from MEMBER where MNUMBER = ?";
 
-		try {
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, mNumber);
-			rs = pstmt.executeQuery();
+        try {
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, mNumber);
+            rs = pstmt.executeQuery();
 
-			if (rs.next()) {
-				return rs.getString("PHONE");
-			}
+            if (rs.next()) {
+                return rs.getString("PHONE");
+            }
 
-			return null;
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		} finally {
-			JdbcUtil.close(rs);
-			JdbcUtil.close(pstmt);
-		}
-	}
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            JdbcUtil.close(rs);
+            JdbcUtil.close(pstmt);
+        }
+    }
 
-	public String selectEmail(Connection conn, int mNumber) {
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+    public String selectEmail(Connection conn, int mNumber) {
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
 
-		String sql = "select EMAIL from MEMBER where MNUMBER = ?";
+        String sql = "select EMAIL from MEMBER where MNUMBER = ?";
 
-		try {
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, mNumber);
-			rs = pstmt.executeQuery();
+        try {
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, mNumber);
+            rs = pstmt.executeQuery();
 
-			if (rs.next()) {
-				return rs.getString("EMAIL");
-			}
+            if (rs.next()) {
+                return rs.getString("EMAIL");
+            }
 
-			return null;
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		} finally {
-			JdbcUtil.close(rs);
-			JdbcUtil.close(pstmt);
-		}
-	}
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            JdbcUtil.close(rs);
+            JdbcUtil.close(pstmt);
+        }
+    }
 
-	/*
-	 * //리팩토링예정 public MemberInfo findMemberInfo(Connection conn, int mNumber) {
-	 * Statement stmt = null; ResultSet rs = null;
-	 * 
-	 * String sql = "select * from MEMBER where MNUMBER =" + mNumber;
-	 * 
-	 * try { stmt = conn.createStatement(); rs = stmt.executeQuery(sql);
-	 * 
-	 * return makeDetailForm(rs); } catch (SQLException e) { throw new
-	 * RuntimeException(e); } }
-	 * 
-	 * private MemberInfo makeDetailForm(ResultSet rs) throws SQLException { while
-	 * (rs.next()) { String name = rs.getString("NAME"); Date birth =
-	 * rs.getDate("BIRTH"); String phone = rs.getString("PHONE"); String email =
-	 * rs.getString("EMAIL");
-	 * 
-	 * return new Member(name, birth, phone, email); } return null; }
-	 */
+    
+//TODO     리팩토링예정 public MemberInfo findMemberInfo(Connection conn, int mNumber) {
+//     Statement stmt = null; ResultSet rs = null;
+//     
+//     String sql = "select * from MEMBER where MNUMBER =" + mNumber;
+//     
+//     try { stmt = conn.createStatement(); rs = stmt.executeQuery(sql);
+//     
+//     return makeDetailForm(rs); } catch (SQLException e) { throw new
+//     RuntimeException(e); } }
+//     
+//     private MemberInfo makeDetailForm(ResultSet rs) throws SQLException { while
+//     (rs.next()) { String name = rs.getString("NAME"); Date birth =
+//     rs.getDate("BIRTH"); String phone = rs.getString("PHONE"); String email =
+//     rs.getString("EMAIL");
+//      
+//     return new Member(name, birth, phone, email); } return null; }
 }
