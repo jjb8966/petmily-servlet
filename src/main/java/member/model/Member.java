3e76a14f871b667b7c0b91bffcc089488a818303
jjb8966 <jjb8966@naver.com>
2022-07-06 22:22:00 +0900

    feat: Member, User 클래스에 mNumber 추가

--- src/main/java/member/model/Member.java
+++ src/main/java/member/model/Member.java
@@ -4,6 +4,7 @@ import java.sql.Date;
 
 public class Member {
 
+	private int mNumber;
 	private String id;
 	private String pw;
 	private String name;
@@ -22,34 +23,70 @@ public class Member {
 		this.phone = phone;
 	}
 
+	public int getmNumber() {
+		return mNumber;
+	}
+
+	public void setmNumber(int mNumber) {
+		this.mNumber = mNumber;
+	}
+
 	public String getId() {
 		return id;
 	}
-	
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
 	public String getPw() {
 		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
 	}
 
 	public String getName() {
 		return name;
 	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public Date getBirth() {
 		return birth;
+	}
+
+	public void setBirth(Date birth) {
+		this.birth = birth;
 	}
 
 	public String getGender() {
 		return gender;
 	}
 
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
 	public String getEmail() {
 		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getPhone() {
 		return phone;
 	}
-	
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
 	public boolean matchPw(String pwd) {
 		return pw.equals(pwd);
 	}
@@ -57,6 +94,4 @@ public class Member {
 	public void changePw(String newPwd) {
 		this.pw = newPwd;
 	}
-
-
 }
